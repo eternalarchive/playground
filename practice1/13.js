13906eebd8c804e7287e98950c3e753daac4b752
eternalarchive <unchd26@gmail.com>
2019-10-10 16:23:18 +0900

    modify 13.js

--- practice1/13.js
+++ practice1/13.js
@@ -3,9 +3,19 @@
 // 개행문자(‘\n’)를 사용하여 개행한다. 완성된 문자열의 마지막은 개행문자(‘\n’)로 끝나도 관계없다.
 var line = 5;
 
+// for (var i = 0; i < line; i++) {
+//   var star = '';
+//   for (var j = line; j > i; j--) {
+//     star += '*';
+//   }
+//   star += '\n';
+//   console.log(star);
+// }
+
+// 둘 다 증가식으로 삼각형 출력(가독성 상대적으로 좋음)
 for (var i = 0; i < line; i++) {
   var star = '';
-  for (var j = line; j > i; j--) {
+  for (var j = 0; i + j < line; j++) {
     star += '*';
   }
   star += '\n';
