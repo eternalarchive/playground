27d6a861057a75064fe71109f8766b5403e8ffb4
eternalarchive <unchd26@gmail.com>
2019-10-29 15:07:15 +0900

    modify practice2/2.js

--- practice2/2.js
+++ practice2/2.js
@@ -5,16 +5,32 @@
 
 // (hint) 문자열 중 n번째에 있는 문자 : str.charAt(n) or str[n]
 
+// 선생님 말씀 : 이중 for문 말고 단일 for문으로 사용해보자, 지금은 찍으면서 이어붙이는거고 찍는거 따로 잇는거 따로
+// 선생님 말씀 : for문 내 조건문 건들지 말고 해보자
+
 function getCount8 () {
   let count = 0;
+  let num = '';
   for (let i = 0; i <= 10000; i++) {
-    for (let j = 0; j < (i + '').length; j++) {
-      // console.log(i + '');
-      if ((i + '').charAt(j) === '8') ++count;
-      // console.log(count);
-    }
+    num += i;
+  }
+  for (let j = 0; j < num.length; j++) {
+    // console.log(num.charAt(j));
+    if (num.charAt(j) === '8') ++ count;
   }
   return count;
 }
+
+// function getCount8 () {
+//   let count = 0;
+//   for (let i = 0; i <= 10000; i++) {
+//     for (let j = 0; j < (i + '').length; j++) {
+//       // console.log(i + '');
+//       if ((i + '').charAt(j) === '8') ++count;
+//       // console.log(count);
+//     }
+//   }
+//   return count;
+// }
 
 console.log(getCount8()); // 4000
